be540d019a208221fd6ee1710cd89c3a20ece269
CalvinWan <t110820058@ntut.org.tw>
2024-01-07 20:59:19 +0800

    Finished move command.

--- PowerPoint/model/command/MoveCommand.cs
+++ PowerPoint/model/command/MoveCommand.cs
@@ -21,6 +21,8 @@ namespace PowerPoint.model.command
         private PointF _point1;
         private PointF _point2;
 
+        private int _slideIndex;
+
         public MoveCommand(Model model, int targetIndex, PointF startPoint, PointF endPoint, PointF pointRecord1, PointF pointRecord2, PointF originDrawPoint1, PointF originDrawPoint2) : base(model)
         {
             _targetIndex = targetIndex;
@@ -34,6 +36,7 @@ namespace PowerPoint.model.command
                 _originDrawPoint1 = originDrawPoint1;
                 _originDrawPoint2 = originDrawPoint2;
             }
+            _slideIndex = model.SlideIndex;
         }
 
         // execute
@@ -41,30 +44,30 @@ namespace PowerPoint.model.command
         {
             _point1 = _originPoint1 + new SizeF(_endPoint.X - _startPoint.X, _endPoint.Y - _startPoint.Y);
             _point2 = _originPoint2 + new SizeF(_endPoint.X - _startPoint.X, _endPoint.Y - _startPoint.Y);
-            _model.GetListOfShape()[_targetIndex].Point1 = _point1;
-            _model.GetListOfShape()[_targetIndex].Point2 = _point2;
-            if (_model.GetListOfShape()[_targetIndex] is Line)
+            _model.GetListOfShape(_slideIndex)[_targetIndex].Point1 = _point1;
+            _model.GetListOfShape(_slideIndex)[_targetIndex].Point2 = _point2;
+            if (_model.GetListOfShape(_slideIndex)[_targetIndex] is Line)
             {
-                Line line = (Line)_model.GetListOfShape()[_targetIndex];
+                Line line = (Line)_model.GetListOfShape(_slideIndex)[_targetIndex];
                 line.DrawPoint1 = _originDrawPoint1 + new SizeF(_endPoint.X - _startPoint.X, _endPoint.Y - _startPoint.Y);
                 line.DrawPoint2 = _originDrawPoint2 + new SizeF(_endPoint.X - _startPoint.X, _endPoint.Y - _startPoint.Y);
             }
-            _model.GetListOfShape()[_targetIndex].Information = string.Format(TEMPLATE, (int)this._point1.X, (int)this._point1.Y) + COMMA + string.Format(TEMPLATE, (int)_point2.X, (int)_point2.Y);
+            _model.GetListOfShape(_slideIndex)[_targetIndex].Information = string.Format(TEMPLATE, (int)this._point1.X, (int)this._point1.Y) + COMMA + string.Format(TEMPLATE, (int)_point2.X, (int)_point2.Y);
             _model.NotifyModelChanged();
         }
 
         // unexecute
         public override void ExecuteBack()
         {
-            _model.GetListOfShape()[_targetIndex].Point1 = _originPoint1;
-            _model.GetListOfShape()[_targetIndex].Point2 = _originPoint2;
-            if (_model.GetListOfShape()[_targetIndex] is Line)
+            _model.GetListOfShape(_slideIndex)[_targetIndex].Point1 = _originPoint1;
+            _model.GetListOfShape(_slideIndex)[_targetIndex].Point2 = _originPoint2;
+            if (_model.GetListOfShape(_slideIndex)[_targetIndex] is Line)
             {
-                Line line = (Line)_model.GetListOfShape()[_targetIndex];
+                Line line = (Line)_model.GetListOfShape(_slideIndex)[_targetIndex];
                 line.DrawPoint1 = _originDrawPoint1;
                 line.DrawPoint2 = _originDrawPoint2;
             }
-            _model.GetListOfShape()[_targetIndex].Information = string.Format(TEMPLATE, (int)this._originPoint1.X, (int)this._originPoint1.Y) + COMMA + string.Format(TEMPLATE, (int)_originPoint2.X, (int)_originPoint2.Y);
+            _model.GetListOfShape(_slideIndex)[_targetIndex].Information = string.Format(TEMPLATE, (int)this._originPoint1.X, (int)this._originPoint1.Y) + COMMA + string.Format(TEMPLATE, (int)_originPoint2.X, (int)_originPoint2.Y);
             _model.NotifyModelChanged();
         }
     }
